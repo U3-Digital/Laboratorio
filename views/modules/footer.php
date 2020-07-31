baae2ecd76e97aa53bf930478d19ce0687a491d1
RayadoxAV <49849531+RayadoxAV@users.noreply.github.com>
2020-07-30 18:58:09 -0600

    Reinicio

--- views/modules/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="sticky-footer">
-    <div class="container">
-    <div class="text-center">
-        <small>Copyright © Your Website <?php echo date('Y'); ?></small>
-    </div>
-    </div>
-</footer>
