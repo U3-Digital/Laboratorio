0143424a7b89c1c7f34429ca197524b12a695061
RayadoxAV <49849531+RayadoxAV@users.noreply.github.com>
2020-07-31 17:07:23 -0600

    Mover las datatables

--- views/modules/inicio.php
+++ views/modules/inicio.php
@@ -26,7 +26,9 @@ require "../../models/crud.php";
 	<script src="../../lib/vendor/sweetalert2/sweetalert2.min.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<!-- data tables-->
 	 <script src="../../lib/vendor/datatables.net/js/jquery.dataTables.min.js"></script>
 	<script src="../../lib/vendor/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
@@ -44,9 +46,7 @@ include "menu.php";
 
 ?>
 
-	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-  crossorigin="anonymous" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	
 	<script src="../../lib/vendor/bootstrap/js/bootstrap.js"></script>
 	<script type="text/javascript" src="../../lib/vendor/adminlte/dist/js/adminlte.js"></script>
 	<script type="text/javascript" src="../js/sb-admin.min.js"></script>
