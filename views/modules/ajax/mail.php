0aa470437723175c5822f23a383151b434d4fc65
RayadoxAV <49849531+RayadoxAV@users.noreply.github.com>
2020-10-10 11:29:59 -0600

    Descomentar la línea que envía el email

--- views/modules/ajax/mail.php
+++ views/modules/ajax/mail.php
@@ -38,14 +38,11 @@ if (isset($_POST["emailCliente"])) {
 
     $subject = "Resultados de estudio para" . $_POST["nombreCliente"];
 
-    /* if (mail($recipient, $subject, $email_content, $headers)) {
+    if (mail($recipient, $subject, $email_content, $headers)) {
         echo "success";
     } else {
        echo "error";
-    }  */
-
-    print_r("hey");
-
+    } 
     // Get the form fields and remove whitespace.
     /* $name = strip_tags(trim($_POST["cliente"]));
     $doctor = trim($_POST["medico"]);
